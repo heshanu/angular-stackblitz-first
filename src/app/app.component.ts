8c4194fe1d155ab327dfe99e0da330e232272924
heshanu <heshanu97@gmail.com>
2022-01-23 10:29:36 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';age=24;
+
+  a=[1,2,3,4,5];
+}
